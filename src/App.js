355c2c5de8c380deccc3b566d7aec1e5df97a4d2
Cozmo <mohamed@lenovo.local>
2021-03-05 18:02:50 +0200

    added routing navigation system

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './App.css';
 import HomePage from "../src/pages/homepage/homepage.component.jsx"
+import { Route, Link } from "react-router-dom";
+
+const Test = (props)=> {
+  console.log(props)
+  return <div>
+    <h1>this is a test page</h1>
+    <Link to={`${props.match.url}/13`}>to topic 13</Link>
+    <Link to={`${props.match.url}/17`}>to topic 17][</Link>
+  </div>
+
+}
+
+const TestId = (props)=>{
+  console.log(props)
+  return <div>
+    <h1>this is a test id and the id is {props.match.params.testid} </h1>
+    <button onClick={()=> props.history.push('/test')}>to test</button>
+    
+  </div>
+}
+  
+
+
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/test' component={Test} />
+      <Route exact path='/test/:testid' component={TestId} />
+
+
+
     </div>
   );
 }
--- src/components/menu-item/menuitem.component.jsx
+++ src/components/menu-item/menuitem.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import "./menu-item.styles.scss"
+import "./menu-item.styles.scss";
+import { withRouter } from "react-router";
 
 const MenuItem = (props) => (
             <div 
                  className= {`${props.size} menu-item`}>
                 <div style={{
                 backgroundImage: `url(${props.imageUrl})`
-                }} className="background-image" />
+                }} className="background-image"
+                onClick={()=>props.history.push(`${props.match.url}${props.linkUrl}`) }
+                 />
                     <div className="content">
                        <h1 className="title">{props.title.toUpperCase()}</h1>
                       <span className="subtitle">subtitle1</span>
@@ -14,4 +17,4 @@ const MenuItem = (props) => (
                 
             </div>
 )
-export default MenuItem;    
+export default withRouter(MenuItem);    
