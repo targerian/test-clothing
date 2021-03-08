56818713c867b14e2eb20a0c2bf0284253dc5142
Mohamed Gamal <mohamedelsonny5@gmail.com>
2021-03-08 22:24:40 +0200

    try fix login

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -1,22 +1,21 @@
-import React from "react"; 
+import React from "react";
 import "./header.styles.scss";
-import { Link } from "react-router-dom"
-import { ReactComponent as Logo } from "../../assets/crown.svg"
-
+import { Link } from "react-router-dom";
+import Logo from "../../assets/crown.svg";
 
 const Header = () => (
-    <div className="header">
-    <Link className="logo-container" to="/" >
-    <Logo className="logo"/>
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <img src={Logo} className="logo" />
     </Link>
     <div className="options">
-        <Link className="option"  to="/shop">
-            Shop
-        </Link>
-        <Link className="option"  to="/contact">
-            Contact
-        </Link>
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
     </div>
-    </div>
-)
+  </div>
+);
 export default Header;
--- src/components/menu-item/menuitem.component.jsx
+++ src/components/menu-item/menuitem.component.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./menu-item.styles.scss";
-import { withRouter } from "react-router";
+import { withRouter } from "react-router-dom";
 
 const MenuItem = (props) => (
-            <div 
-                 className= {`${props.size} menu-item`}>
-                <div style={{
-                backgroundImage: `url(${props.imageUrl})`
-                }} className="background-image"
-                onClick={()=>props.history.push(`${props.match.url}${props.linkUrl}`) }
-                 />
-                    <div className="content">
-                       <h1 className="title">{props.title.toUpperCase()}</h1>
-                      <span className="subtitle">subtitle1</span>
-                     </div>
-                
-            </div>
-)
-export default withRouter(MenuItem);    
+  <div className={`${props.size} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${props.imageUrl})`,
+      }}
+      className="background-image"
+      onClick={() => props.history.push(`${props.match.url}${props.linkUrl}`)}
+    />
+    <div className="content">
+      <h1 className="title">{props.title.toUpperCase()}</h1>
+      <span className="subtitle">subtitle1</span>
+    </div>
+  </div>
+);
+export default withRouter(MenuItem);
