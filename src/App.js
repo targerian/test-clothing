83b8c5c0834f96daf1f35f6204e53f555de40414
Cozmo <mohamed@lenovo.local>
2021-03-19 13:24:22 +0200

    added redirect to home afer signing in

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
 import "./App.css";
 import HomePage from "../src/pages/homepage/homepage.component.jsx";
-import { Route, Link, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ShopPage from "./pages/shoppage/shop.component";
 import Header from "./components/header-component/header.component";
 import SignInAndSignUp from "./pages/signin-and-signup/signin-and-signup.component";
 import { auth, CreateUserProfileDocument } from "./firebase/firebase.util";
 import { setCurrentUser } from "./redux/user/user.action";
 import { connect } from "react-redux";
-const Test = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>this is a test page</h1>
-      <Link to={`${props.match.url}/13`}>to topic 13</Link>
-      <Link to={`${props.match.url}/17`}>to topic 17][</Link>
-    </div>
-  );
-};
-
-const TestId = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>this is a test id and the id is {props.match.params.testid} </h1>
-      <button onClick={() => props.history.push("/test")}>to test</button>
-    </div>
-  );
-};
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -39,10 +19,9 @@ class App extends React.Component {
       if (userAuth) {
         userRef.onSnapshot((snapshot) => {
           setCurrentUser({
-              id: snapshot.id,
-              ...snapshot.data(),
-            }
-          );
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
         });
       } else {
         setCurrentUser(userAuth);
@@ -59,18 +38,26 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route exact path="/test" component={Test} />
-          <Route exact path="/test/:testid" component={TestId} />
           <Route exact path="/shop" component={ShopPage} />
-          <Route exact path="/sign" component={SignInAndSignUp} />
+          <Route
+            exact
+            path="/sign"
+            render={() =>
+              this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -1,10 +1,11 @@
+import {UserActionType} from "./user.action.type"
 const INITIAL_STATE = {
     currentUser : null
 }
 
 const userReducer = (state = INITIAL_STATE,action) => {
     switch (action.type){
-        case 'SET_CURRENT_USER':
+        case UserActionType.SET_CURRENT_USER :
             return {
                 ...state,
                 currentUser: action.payload
