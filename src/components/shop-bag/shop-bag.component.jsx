d2cffb05428621c84588ef812c693f0fa04c668a
Cozmo <mohamed@lenovo.local>
2021-03-28 15:54:42 +0200

    created cart-item component and connected it with cartItems  reducers, tried to make the algorithm for the bagpage to display the right quaninty but its still under development

--- src/components/shop-bag/shop-bag.component.jsx
+++ src/components/shop-bag/shop-bag.component.jsx
@@ -3,10 +3,14 @@ import "./shop-bag.styles.scss"
 import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon} from "../../assets/11.2 shopping-bag.svg"
 import { ToggleCartvisibility } from "../../redux/cart/cart.action"
-const CartIcon = ({ToggleCartvisibility}) => (
+const CartIcon = ({ToggleCartvisibility, cartItems}) => (
     <div className="cart-icon" onClick={ToggleCartvisibility}>
         <ShoppingIcon className="shopping-icon "/>
-        <span className="item-count">0</span>
+        <span className="item-count">
+            {cartItems.map(cartItem =>(
+                cartItem.quantity
+            ))}
+        </span>
     </div>
 )
 
@@ -14,5 +18,9 @@ const mapDispatchToProps = dispatch => ({
     ToggleCartvisibility: ()=> dispatch(ToggleCartvisibility())
 })
 
+const mapStateToProps = ({cart:{cartItems}}) => ({
+    cartItems,
+})
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
