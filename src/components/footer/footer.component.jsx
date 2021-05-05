c4a059b8227bbb5ead91b58b1875fe660a66b849
Cozmo <mohamed@lenovo.local>
2021-05-05 21:52:58 +0200

    added responsivness to mobile apps, also a footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -3,7 +3,7 @@ import "./footer.styles.scss"
 
 const Footer = () => (
     <div className="footer">
-        COPYRIGHTS: MOHAMED ALI™ @2021
+        COPYRIGHTED BY: MOHAMED ALI™ @2021
     </div>
 )
 export default Footer;
